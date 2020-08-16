11199392791ed2397038bae57b6d1f64fd2f1df3
smileamaxdev <novoseltsev.as@phystech.edu>
2020-08-16 13:56:35 +0300

    4 part done

--- 4.Images_and_Large_Array_Types/4/4.cpp
+++ 4.Images_and_Large_Array_Types/4/4.cpp
@@ -1,17 +1,14 @@
 ﻿#include <opencv2/opencv.hpp>
-#include <iostream>
-int main() {
-	size_t c = 300, r = 300;
-	cv::Mat source(c, r, 1.f);
-	cv::Mat integrate(c, r, 0.f);
-	cv::RNG rng(0x1234);
-	rng.fill(source, cv::RNG::NORMAL, 0, 1);
-	cv::namedWindow("Source", cv::WINDOW_AUTOSIZE);
-	//cv::namedWindow("Integrate", cv::WINDOW_AUTOSIZE);
-	//integrate.at<float>(0, 0) = 0;
+
+cv::Mat Integrate(const cv::Mat& source) {
+	size_t c = source.cols;
+	size_t r = source.rows;
+	int type = source.type();
+	cv::Mat integrate(c, r, type);
+	integrate.at<float>(0, 0) = 0;
 	float last_sum_col = 0, last_sum_row = 0;
-	for (int i = 0; i < source.cols; i++) {
-		for (int j = 0; j < source.rows; j++) {
+	for (int i = 0; i < source.rows; i++) {
+		for (int j = 0; j < source.cols; j++) {
 			if ((i == 0) && (j != 0)) {
 				integrate.at<float>(0, j) = last_sum_col + source.at<float>(0, j - 1);
 				last_sum_col = integrate.at<float>(0, j);
@@ -20,18 +17,27 @@ int main() {
 				integrate.at<float>(i, 0) = last_sum_row + source.at<float>(i - 1, 0);
 				last_sum_row = integrate.at<float>(i, 0);
 			}
-			else if(i !=0 && j !=0) {
-				float sum1 = integrate.at<float>(i, j - 1) + integrate.at<float>(i - 1, j);
-				float sum2 = sum1 - integrate.at<float>(i - 1, j - 1);
-				//std::cout << sum << std::endl;
-				integrate.at<float>(i, j) = sum2;
-			}
+			else if (i != 0 && j != 0)
+				integrate.at<float>(i, j) = integrate.at<float>(i, j - 1) + integrate.at<float>(i - 1, j) - integrate.at<float>(i - 1, j - 1);
 		}
 	}
-	
-	cv::imshow("Source", source);
-	//cv::imshow("Integrate", integrate);
-	cv::waitKey(0);
+	return integrate;
+}
+
+
+int main() {
+	cv::Mat source(400, 400, CV_32F);
+	cv::RNG rng(std::time(0));
+	rng.fill(source, cv::RNG::NORMAL, 0.f, 1000.f);
+	cv::namedWindow("Source", cv::WINDOW_NORMAL);
+	cv::namedWindow("Integrate", cv::WINDOW_NORMAL);
+	cv::Mat integrate = Integrate(source);
+	char c = '1';
+	while (c != 'c') {
+		cv::imshow("Source", source);
+		cv::imshow("Integrate", integrate);
+		c = cv::waitKey(0);
+	}
 	return 0;
 }
 
