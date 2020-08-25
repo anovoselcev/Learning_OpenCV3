d8a4daa65a281451f6f040e5d0da5c5e63384d52
smileamaxdev <novoseltsev.as@phystech.edu>
2020-08-26 00:02:58 +0300

    Circle done

---
+++ 5.Array_Operations/2/2.cpp
@@ -0,0 +1,12 @@
+﻿#include <opencv2/opencv.hpp>
+#include <iostream>
+
+int main() {
+	cv::Mat_<uchar> img = cv::Mat_<uchar>::zeros(400, 400);
+	cv::namedWindow("Circle");
+	cv::circle(img, cv::Point(100, 100), 80, uchar(100));
+	cv::imshow("Circle", img);
+	cv::waitKey(0);
+	return 0;
+}
+
