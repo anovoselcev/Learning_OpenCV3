744ff84fd1a8b5cf83e9f665ce877a98d651ed74
smileamaxdev <novoseltsev.as@phystech.edu>
2020-08-27 01:52:56 +0300

    3 done

---
+++ 5.Array_Operations/3/3.cpp
@@ -0,0 +1,29 @@
+﻿#include <opencv2/opencv.hpp>
+#include <iostream>
+
+void PrintRectFor(cv::Mat& img, const cv::Point& lhs, const cv::Point& rhs) {
+	for (int i = lhs.y; i <= rhs.y; i++) {
+		std::cout << i << std::endl;
+		img.at<cv::Vec3b>(i, rhs.x + 1) = { 200, 0, 100 };
+		img.at<cv::Vec3b>(i, lhs.x + 1) = { 200, 0, 100 };
+	}
+	
+	for (int i = lhs.x; i <= rhs.x; i++) {
+		img.at<cv::Vec3b>(lhs.y, i) = { 200, 0, 100 };
+		img.at<cv::Vec3b>(rhs.y, i) = { 200, 0, 100 };
+	}
+
+
+}
+
+
+int main() {
+	cv::Mat img = cv::Mat::zeros(cv::Size(100, 100), CV_16UC3);
+	cv::Point p1(20, 5), p2(40, 20);
+	PrintRectFor(img, p1, p2);
+	cv::namedWindow("Window", cv::WINDOW_NORMAL);
+	cv::imshow("Window", img);
+	cv::waitKey(0);
+	return 0;
+}
+
