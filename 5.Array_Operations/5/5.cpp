a79a8124c246af3f88e3f02cef6a89aa00feb3f1
smileamaxdev <novoseltsev.as@phystech.edu>
2020-08-27 21:25:12 +0300

    bitwise_not with roi done

---
+++ 5.Array_Operations/5/5.cpp
@@ -0,0 +1,18 @@
+﻿#include <opencv2/opencv.hpp>
+
+int main() {
+	cv::Mat img = cv::imread("../../image3.png");
+	cv::Mat roi1 = cv::Mat::zeros(cv::Size(img.cols, img.rows), CV_8U);
+	cv::Mat roi2 = cv::Mat::zeros(cv::Size(img.cols, img.rows), CV_8U);
+	cv::rectangle(roi1, cv::Rect(cv::Point(5, 10),
+								 cv::Point(25, 40)), 1);
+	cv::rectangle(roi2, cv::Rect(cv::Point(50, 60),
+						cv::Point(70, 90)), 1);
+	cv::bitwise_not(img, img, roi1);
+	cv::bitwise_not(img, img, roi2);
+	cv::namedWindow("Window");
+	cv::imshow("Window", img);
+	cv::waitKey(0);
+	return 0;
+}
+
