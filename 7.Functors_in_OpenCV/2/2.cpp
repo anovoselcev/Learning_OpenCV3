85b8b9f0afbe0f81ee27637379a5e36547778705
smileamaxdev <novoseltsev.as@phystech.edu>
2020-09-12 01:03:08 +0300

    rng fill

---
+++ 7.Functors_in_OpenCV/2/2.cpp
@@ -0,0 +1,14 @@
+﻿#include <opencv2/opencv.hpp>
+
+int main() {
+	cv::Mat mat1 = cv::Mat::zeros(cv::Size(20, 1), CV_32F);
+	cv::RNG rng;
+	rng.fill(mat1, cv::RNG::UNIFORM, 0., 1.);
+	rng.fill(mat1, cv::RNG::NORMAL, 0., 1.);
+	cv::Mat mat2 = cv::Mat::zeros(cv::Size(20, 1), CV_16U);
+	rng.fill(mat2, cv::RNG::UNIFORM, 0, 255);
+	cv::Mat mat3 = cv::Mat::zeros(cv::Size(20, 1), CV_16UC3);
+	rng.fill(mat3, cv::RNG::UNIFORM, 0, 255);
+	return 0;
+}
+
