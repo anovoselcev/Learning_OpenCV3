d009081ac9fc3eb3dc2e41405bef176df1d62ee2
smileamaxdev <novoseltsev.as@phystech.edu>
2020-09-14 23:49:17 +0300

    simple PCA

---
+++ 7.Functors_in_OpenCV/3/3.cpp
@@ -0,0 +1,20 @@
+﻿#include <opencv2/opencv.hpp>
+#include <ctime>
+#include <vector>
+#include <iostream>
+
+int main() {
+	std::vector<cv::Mat> mat(3, cv::Mat::zeros(cv::Size(1, 100), CV_16U));
+	cv::RNG rng(std::time(0));
+	rng.fill(mat[0], cv::RNG::NORMAL, 64, 10);
+	rng.fill(mat[1], cv::RNG::NORMAL, 192, 10);
+	rng.fill(mat[2], cv::RNG::NORMAL, 128, 2);
+	cv::Mat result(100, 3, CV_16U);
+	for (int i = 0; i < 3; i++)
+		mat[i].copyTo(result.col(i));
+	cv::PCA pca;
+	auto m = pca(result, cv::noArray(), cv::PCA::DATA_AS_COL, 2);
+	auto mean = pca.mean;
+	return 0;
+}
+
